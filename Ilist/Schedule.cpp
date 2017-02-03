967d67fde9f11331fe9d14cca214a378987958d1
Josh Graham <jgrah001@odu.edu>
2017-02-03 14:24:43 -0500

    Addition of a courses IList assignment.

---
+++ Ilist/Schedule.cpp
@@ -0,0 +1,93 @@
+#include <utility>
+
+#include "Schedule.h"
+
+using namespace std::rel_ops;
+
+/**
+ *
+ */
+Schedule::Schedule()
+    :head(nullptr), 
+     tail(nullptr),
+     totalCredits(0)
+{
+}
+
+Schedule::Schedule(Schedule const& hold)
+{
+
+}
+
+Schedule::~Schedule()
+{
+
+}
+
+
+/**
+ *
+ */
+bool Schedule::add(Course course)
+{
+    if (head==nullptr)
+    {
+        Node* hold = new Node(course);
+        head = hold;
+        tail = hold;
+        totalCredits += course.getCredits();
+        return true;
+    }
+    Node* itr = head;
+    while( itr != nullptr) 
+    {
+        if(course.getNumber() == itr->data.getNumber())
+        {
+            return false;
+        }
+        itr = itr->next;
+    }
+    if(totalCredits + course.getCredits() <= 12)
+    {
+        Node* hold = new Node(course);
+        tail->next=hold;
+        tail = tail->next;
+        totalCredits += course.getCredits();
+        return true;
+    }
+    return false;
+}
+
+/**
+ *
+ */
+void Schedule::display(std::ostream& outs) const
+{
+    Node* it = head;
+
+    outs << "  (" << totalCredits << " Total Credits)" << "\n";
+
+    while (it != nullptr) {
+        // oops I forgot to remove most of this function    "$NAME (CRN $CRN)"
+        outs << "  - " << it->data.getCredits() << " credits - " << it->data.getNumber() << " (CRN " << it->data.getCrn() << ")\n";
+        it = it->next;
+    }    
+}
+
+
+
+/**
+ * You may ignore this function, or use it, the choice is yours
+ */
+void Schedule::deAllocateList()
+{
+
+}
+
+/**
+ * You may ignore this function, or use it, the choice is yours
+ */
+void Schedule::copyList(const Schedule& rhs)
+{
+
+}
